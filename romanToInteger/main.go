327ae34f421a79965f669ba033a2b03513f833df
LiuJianjun <jianjun@dappworks.cn>
2018-09-06 08:50:57 +0800

    add romean to integer

--- romanToInteger/main.go
+++ romanToInteger/main.go
@@ -1,8 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+    fmt.Println(romanToInt("LVIII"))
 }
 func romanToInt(s string) int {
-    return 0
+    var mapData =map[rune]int{
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
+    }
+    result :=0
+    lastValue:=0
+    for _,v :=range s {
+        if lastValue < mapData[v] {
+            result=result+mapData[v]-lastValue*2
+        }else{
+            result=result+mapData[v]
+        }
+        lastValue=mapData[v]
+    }
+    return result
 }
