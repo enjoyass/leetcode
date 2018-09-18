b63526219e27b7ea0ea6f8dc3702446504e9ab4c
LiuJianjun <jianjun@dappworks.cn>
2018-09-18 13:50:00 +0800

    添加json返回

--- blog/service/handlers.go
+++ blog/service/handlers.go
@@ -2,8 +2,16 @@ package service
 
 import (
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
+	
 )
+func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
 
 func GetName(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","application/json;charset=UTF-8")
